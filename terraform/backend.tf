27a9c597f791fc99198610d0744d2929ad6ac3ff
DivyaJyothiVundavalli <68144559+DivyaJyothiVundavalli@users.noreply.github.com>
2024-12-16 14:40:25 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
-    region = "eu-west-3"
-    key = "eks/terraform.tfstate"
+    bucket = "ecomappbucket"
+    key    = "eks/terraform.tfstate"
+    region = "us-east-2"
   }
 }
